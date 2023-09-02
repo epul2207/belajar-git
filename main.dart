05a0c6832017e55b01aa17e41df20321a27dd1ba
epul2207 <epuls073@gmail.com>
2023-09-02 18:47:04 +0700

    Menambahkan file main.dart

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+  print('Ini adalah file untuk belajar git ');
+
+  String nama = 'Ucup';
+  int umur = 29;
+
+  print('Nama saya adalah $nama');
+  print('Umur saya adalah $umur');
+}
